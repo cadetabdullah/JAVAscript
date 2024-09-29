f42afdcb8a78b1758589dc9d9a7a2fab9e2d5a33
cadetabdullah <abdullah.r1232@gmail.com>
2024-09-30 04:12:32 +0500

    no

---
+++ 01_Variables.js
@@ -0,0 +1,9 @@
+const accountId = "cadetabdullahrajput"
+let accountEmail = "abdullah.r1232@gmail.com"
+var accountPassword = "rana 1234"
+
+console.log(`The account id is ${accountId}. your Email is ${accountEmail}. Your password is ${accountPassword}.`);
+
+accountEmail="abdullahtec321@gmail.com"
+accountPassword = "Ali 4321"
+console.log(`\nThe account ID cannot change so The account id is ${accountId}. your Email is ${accountEmail}. Your password is ${accountPassword}.`);
